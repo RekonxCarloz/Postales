168d4f1aa8fcf50acb15d828ce7b4ea7361efeb1
Carlos Cobian <carloscobian@MacBook-Air-de-Carlos.local>
2019-10-29 14:14:50 -0600

    Import confirm, validetta en Header.

--- application/views/header.php
+++ application/views/header.php
@@ -2,20 +2,26 @@
 <html>
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Inicio - iPostal</title>
-    <meta name="description" content="Proyecto Tecnologías para la Web. Postales">
-    <link rel="stylesheet" href="<?php base_url(); ?>assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
-    <link rel="stylesheet" href="<?php base_url(); ?>assets/fonts/simple-line-icons.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
-    <link rel="stylesheet" href="<?php base_url(); ?>assets/css/smoothproducts.css">
-    <link rel="stylesheet" href="<?php base_url(); ?>assets/css/headerPostalCategory.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <title>Inicio - iPostal</title>
+  <meta name="description" content="Proyecto Tecnologías para la Web. Postales">
+  <link rel="stylesheet" href="<?php base_url(); ?>assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+  <link rel="stylesheet" href="<?php base_url(); ?>assets/fonts/simple-line-icons.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+  <link rel="stylesheet" href="<?php base_url(); ?>assets/css/validetta/validetta.min.css">
+  <link rel="stylesheet" href="<?php base_url(); ?>assets/css/smoothproducts.css">
+  <link rel="stylesheet" href="<?php base_url(); ?>assets/css/headerPostalCategory.css">
+  <link rel="stylesheet" href="<?php base_url(); ?>assets/confirm/dist/jquery-confirm.min.css">
 
     <script src="<?php base_url(); ?>assets/js/jquery.min.js"></script>
     <script src="<?php base_url(); ?>assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+    <script src="<?php base_url(); ?>assets/js/validetta/validetta.min.js"></script>
+    <script src="<?php base_url(); ?>assets/js/validetta/validettaLang-es-ES.js"></script>
     <script src="<?php base_url(); ?>assets/js/smoothproducts.min.js"></script>
     <script src="<?php base_url(); ?>assets/js/theme.js"></script>
+    <script src="<?php base_url(); ?>assets/confirm/dist/jquery-confirm.min.js"></script>
+
 </head>
