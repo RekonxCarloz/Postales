859f16a2c9c93fe93611216b5e4f9e4c1e4b161b
Carlos Cobian <carloscobian@MacBook-Air-de-Carlos.local>
2019-11-28 12:14:45 -0600

    cambio base_url footer

--- application/views/footer/footer.php
+++ application/views/footer/footer.php
@@ -4,15 +4,15 @@
               <div class="col-sm-3">
                   <h5>Empieza aquí</h5>
                   <ul>
-                      <li><a href="<?php base_url();?>inicio">Inicio</a></li>
-                      <li><a href="<?php base_url();?>registro">Registrarse</a></li>
+                      <li><a href="<?= base_url();?>inicio">Inicio</a></li>
+                      <li><a href="<?= base_url();?>registro">Registrarse</a></li>
                   </ul>
               </div>
               <div class="col-sm-3">
                   <h5>Acerca de</h5>
                   <ul>
-                      <li><a href="<?php base_url();?>about">Desarrolladores</a></li>
-                      <li><a href="<?php base_url();?>contacto">Contáctanos</a></li>
+                      <li><a href="<?= base_url();?>about">Desarrolladores</a></li>
+                      <li><a href="<?= base_url();?>contacto">Contáctanos</a></li>
                   </ul>
               </div>
           </div>
